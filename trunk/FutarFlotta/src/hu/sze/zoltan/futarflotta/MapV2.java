5a583b8def9a25026f838f663d417b7d19d7f118
szuri90@gmail.com <szuri90@gmail.com@b79b8a45-bed5-ab5d-ae23-dc70330dc022>
2013-05-19 08:54:45 +0000

    fixed issues.

--- trunk/FutarFlotta/src/hu/sze/zoltan/futarflotta/MapV2.java
+++ trunk/FutarFlotta/src/hu/sze/zoltan/futarflotta/MapV2.java
@@ -31,7 +31,6 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 
 public class MapV2 extends FragmentActivity implements
