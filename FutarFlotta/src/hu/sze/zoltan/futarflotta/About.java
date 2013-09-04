f73ff289eea6f9222afccbb2ab0e5aa7f5658e24
szuri90@gmail.com <szuri90@gmail.com@b79b8a45-bed5-ab5d-ae23-dc70330dc022>
2013-09-04 08:08:02 +0000

    About form added. Some class deleted.

---
+++ FutarFlotta/src/hu/sze/zoltan/futarflotta/About.java
@@ -0,0 +1,12 @@
+package hu.sze.zoltan.futarflotta;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class About extends Activity{
+
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.about_activity);
+	}
+}
--- FutarFlotta/src/hu/sze/zoltan/futarflotta/MainActivity.java
+++ FutarFlotta/src/hu/sze/zoltan/futarflotta/MainActivity.java
@@ -13,109 +13,108 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
-	
+
 	private Button btnLogOut;
 	private Button btnMap;
 	private Button btnTasks;
 	private Button btnSettings;
-	private Button btnUsers;
 	public TextView txtViewUser;
 	public String value;
 	public String userName;
 	public String ures = "main";
-	
+
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main_activity);
-		
+
 		btnMap = (Button) findViewById(R.id.btnMap);
 		btnLogOut = (Button) findViewById(R.id.btnLogOut);
 		btnTasks = (Button) findViewById(R.id.btnTasks);
 		btnSettings = (Button) findViewById(R.id.btnSettings);
-		btnUsers = (Button) findViewById(R.id.btnUsers);
+
 		txtViewUser = (TextView) findViewById(R.id.txtFelhasznalo);
-		
+
 		Bundle extras = getIntent().getExtras();
 		if (extras != null) {
-		    value = extras.getString("fullName");
-		    userName = extras.getString("userName");
+			value = extras.getString("fullName");
+			userName = extras.getString("userName");
 		}
-		
+
 		LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-		
+
 		txtViewUser.setText(value);
-		
-		
+
 		if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-			new AlertDialog.Builder(this).setMessage("GPS kötelezõ.").setCancelable(false).setPositiveButton("GPS", new DialogInterface.OnClickListener() {
-				
-				@Override
-				public void onClick(DialogInterface dialog, int which) {
-					Intent callGPSSettingIntent = new Intent(
-							android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-					startActivity(callGPSSettingIntent);
-				}
-			}).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-				
-				@Override
-				public void onClick(DialogInterface dialog, int which) {
-					dialog.cancel();
-					Toast.makeText(getApplicationContext(), "GPS nincs bekapcsolva", Toast.LENGTH_LONG).show();
-				}
-			}).create().show();
+			new AlertDialog.Builder(this)
+					.setMessage("GPS kötelezõ.")
+					.setCancelable(false)
+					.setPositiveButton("GPS",
+							new DialogInterface.OnClickListener() {
+
+								@Override
+								public void onClick(DialogInterface dialog,
+										int which) {
+									Intent callGPSSettingIntent = new Intent(
+											android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+									startActivity(callGPSSettingIntent);
+								}
+							})
+					.setNegativeButton("Cancel",
+							new DialogInterface.OnClickListener() {
+
+								@Override
+								public void onClick(DialogInterface dialog,
+										int which) {
+									dialog.cancel();
+									Toast.makeText(getApplicationContext(),
+											"GPS nincs bekapcsolva",
+											Toast.LENGTH_LONG).show();
+								}
+							}).create().show();
 		}
-		
+
 		btnMap.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
-				Intent myIntent = new Intent(MainActivity.this,MapV2.class);
-//				Intent myIntent = new Intent(MainActivity.this,Map.class);
-//				myIntent.putExtra("address", ures);
+				Intent myIntent = new Intent(MainActivity.this, MapV2.class);
 				startActivity(myIntent);
 			}
 		});
-		
+
 		btnLogOut.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View arg0) {
-				finish();		
+				finish();
 			}
 		});
-		
+
 		btnTasks.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
-				Intent myIntent = new Intent(MainActivity.this,TasksActivity.class);
+				Intent myIntent = new Intent(MainActivity.this,
+						TasksActivity.class);
 				myIntent.putExtra("userName", userName);
 				startActivity(myIntent);
 			}
 		});
-		
+
 		btnSettings.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
-				Intent myIntent = new Intent (MainActivity.this, Settings.class);
+				Intent myIntent = new Intent(MainActivity.this, Settings.class);
 				myIntent.putExtra("userName", userName);
-				startActivity(myIntent);		
+				startActivity(myIntent);
 			}
 		});
-		
-		btnUsers.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				Intent myIntent = new Intent (MainActivity.this, UsersActivity.class);
-				startActivity(myIntent);		
-			}
-		});
+
 	}
-	
+
 	@Override
 	public void onBackPressed() {
-	    // do nothing.
+		// do nothing.
 	}
 }
